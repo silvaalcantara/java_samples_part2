0264970e4ab6a89ea3f9338aaec3d2703c3d9f50
silvaalcantara <allan.pa.br@gmail.com>
2020-06-24 09:40:58 -0300

    override and super examples

--- inheritance/AccountOperation.java
+++ inheritance/AccountOperation.java
@@ -8,13 +8,23 @@ public class AccountOperation {
 
     public static void main(String[] args) {
         
-        Account acc = new Account(1001, "Alex", 0.0);
+        Account acc = new Account(1001, "Alex", 1000.0);
+        acc.withdraw(200.0);
         BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        System.out.println("Acc balance: " + acc.getBalance()); 
 
         // UPCASTING
         Account acc1 = bacc;
-        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+        Account acc2 = new BusinessAccount(1003, "Bob", 500.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        acc3.withdraw(200.0);
+
+        System.out.println("Acc2 balance: " + acc2.getBalance()); 
+        System.out.println("Acc3 balance: " + acc3.getBalance()); 
+
+        System.out.println("Acc1 balance: " + acc1.getBalance()); 
 
         // DOWNCASTING
         BusinessAccount acc4 = (BusinessAccount) acc2;
